a52cf304254b60912bca85b91ac19cccf9b0ee12
dbm1982 <dbm1982@Hotmail.com>
2026-02-03 13:16:04 -0500

    Update U10G_Schedule_Session2.py

--- U10G_Schedule_Session2.py
+++ U10G_Schedule_Session2.py
@@ -32,7 +32,7 @@ RESOURCE_AREA_MAP = {
 
 VENUE_ADDRESS = "Union Point Sports Complex, 170 Memorial Grove Ave, Weymouth, MA 02190"
 
-TEAM_PAGE_URL = "https://apps.daysmartrecreation.com/dash/x/#/online/unionpointsports/teams/574"
+TEAM_PAGE_URL = "https://apps.daysmartrecreation.com/dash/x/#/online/unionpointsports/teams/2640"
 
 
 def fetch_league_data():
